9f27f995fbc1400a4979c90faa522dfeb6f93d5a
lebo.mofokeng <lebo.mofokeng@DDNAD4R7DB22>
2022-06-30 12:40:31 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            WriteLine("Gueess a number between 1 and 9");
+
+            num =Convert.ToInt32(ReadLine());
+
+
+            Random randNum = new Random();
+            
+            int rNum = randNum.Next(1, 10);
+
+            if(num == rNum)
+            {
+                WriteLine("well done. Your guess is correct!");
+            }
+            if (num < rNum)
+            {
+                WriteLine("Your guess is low!");
+            }
+
+            if (num > rNum)
+            {
+                WriteLine("Your guess is to high!");
+            }
+            ReadKey();
+        }
+    }
+}
